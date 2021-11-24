4be005da9c8bc89e3e830431f86059a2b15fbd8e
jsilva <silvajonatanivan@gmail.com>
2021-11-24 05:52:00 -0300

    Setting heroku

--- src/db/connection/index.js
+++ src/db/connection/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env // eslint-disable-line
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const { DATABASE_URL } = process.env // eslint-disable-line
+const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false // lets Sequelize know we can use pg-native for ~30% more speed
 });
